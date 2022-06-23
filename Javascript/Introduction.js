91ef437a0806c288dc4ef499dd470e132e03d216
Micheal Etefia <58205721+Mickelstar@users.noreply.github.com>
2022-06-23 18:53:32 +0100

    Add files via upload

---
+++ Javascript/Introduction.js
@@ -0,0 +1,16 @@
+// Name, Height, and Country PRINTED on the screen
+
+const info = "<h1>Introduction to Javascript</h1>";
+
+document.write(info);
+
+const Name = (
+  <h2>
+    names: Etefia Micheal Ezekiel
+    <br />
+    Height: 6'2 ft <br />
+    Country: Nigeria
+  </h2>
+);
+
+document.write(Name);
